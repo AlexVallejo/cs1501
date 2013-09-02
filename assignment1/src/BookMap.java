671559620950b9cfbf504552298b2371f50fbb6c
Alex Vallejo <vallejo.alex@gmail.com>
2013-09-02 15:27:33 -0400

    Implement distanceBetween method

--- assignment1/src/BookMap.java
+++ assignment1/src/BookMap.java
@@ -11,8 +11,8 @@ import java.util.TreeMap;
 
 public class BookMap {
 
-    int lines;
-    TreeMap<String, Integer> map;
+    public int lines;
+    public TreeMap<String, Integer> map;
 
     //TODO handle exceptions with helpful error messages
     public BookMap(String filename) throws Exception{
@@ -20,27 +20,34 @@ public class BookMap {
         map = new TreeMap<String, Integer>();
         String line;
         String[] words;
-        Scanner input = new Scanner(new File(filename));
 
-        while (input.hasNext()){
-            line = input.nextLine();
-            lines++;
-            line = line.replaceAll("[^a-zA-Z0-9\\s]", " ");
-            line = line.toLowerCase();
-            words = line.split(" ");
+        try{
+            Scanner input = new Scanner(new File(filename));
 
-            for (String word : words){
-                if (word.equals(""))
-                    continue;
+            while (input.hasNext()){
+                line = input.nextLine();
+                lines++;
+                line = line.replaceAll("[^a-zA-Z0-9\\s]", " ");
+                line = line.toLowerCase();
+                words = line.split(" ");
 
-                if (map.get(word) == null)
-                    map.put(word,0);
+                for (String word : words){
+                    if (word.equals(""))
+                        continue;
 
-                else
-                    map.put(word, 1 + map.get(word));
+                    if (map.get(word) == null)
+                        map.put(word,1);
+
+                    else
+                        map.put(word, 1 + map.get(word));
+
+                }
 
             }
+        }
 
+        catch (Exception ex){
+            System.out.println(filename + " not found!");
         }
 
         Collection<Integer> wordCollection = map.values();
@@ -55,6 +62,36 @@ public class BookMap {
     }
 
     public double distanceBetween(BookMap otherBook){
-        return 0.0;
+        //return NaN as instructed if the book is not filled
+        if (otherBook.map.size() < 1 || map.size() < 1)
+            return 0.0/0.0;
+
+        Collection<Integer> otherFrequencies = otherBook.map.values();
+        Collection<Integer> frequencies = map.values();
+
+        //Compute the normalizations of the two vectors
+        double norm = 0;
+        double otherNorm = 0;
+
+        for (Integer num : frequencies)
+            norm += (num * num);
+
+        for (Integer num : otherFrequencies)
+            otherNorm += (num * num);
+
+        norm = Math.sqrt(norm);
+        otherNorm = Math.sqrt(otherNorm);
+
+        //Compute the inner product of the numbers
+        double innerProd = 0;
+
+        for (String word : map.keySet()){
+            Integer value = otherBook.map.get(word);
+
+            if (value != null)
+                innerProd += value * map.get(word);
+        }
+
+        return Math.acos(innerProd / (norm * otherNorm));
     }
 }
