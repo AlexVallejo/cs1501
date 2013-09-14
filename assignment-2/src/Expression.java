e6c9682526c4afdc13676fb012c379c048398847
Alex Vallejo <vallejo.alex@gmail.com>
2013-09-14 14:29:49 -0400

    Initial commit of required files for assignment-2

---
+++ assignment-2/src/Expression.java
@@ -0,0 +1,34 @@
+/**
+ * Created By: Alex Vallejo
+ * Date: 9/14/13
+ * Project: assignment-2
+ * Email: amv49@pitt.edu
+ * Peoplesoft: 357-8411
+ */
+
+public class Expression {
+
+    public Expression(){
+    }
+
+    public static void setAtom(String atom, String value){
+    }
+
+    public boolean evaluate(){
+        return false;
+    }
+
+    public Expression copy(){
+        return new Expression();
+    }
+
+    public void normalize(){
+    }
+
+    public void displayNormalized(){
+    }
+
+    public String toString(){
+        return "";
+    }
+}
---
+++ assignment-2/src/ParseError.java
@@ -0,0 +1,10 @@
+/**
+ * Created By: Alex Vallejo
+ * Date: 9/14/13
+ * Project: assignment-2
+ * Email: amv49@pitt.edu
+ * Peoplesoft: 357-8411
+ */
+
+public class ParseError {
+}
