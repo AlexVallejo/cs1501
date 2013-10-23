a1144e2f0b2dd821c6808ceabe81be468c8016d2
Alex Vallejo <vallejo.alex@gmail.com>
2013-10-22 20:12:19 -0400

    Add missing data structures to stdlib

--- stdlib/Bag.java
+++ stdlib/Bag.java
@@ -2,7 +2,7 @@
  *  Compilation:  javac Bag.java
  *  Execution:    java Bag < input.txt
  *
- *  A generic bag or multiset, implemented using a linked list.
+ *  A generic bag or multiset, implemented using a singly-linked list.
  *
  *  % more tobe.txt 
  *  to be or not to - be - - that - - - is
@@ -34,91 +34,80 @@ import java.util.NoSuchElementException;
  *  generic items. It supports insertion and iterating over the 
  *  items in arbitrary order.
  *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation 
+ *  This implementation uses a singly-linked list with a static nested class Node.
+ *  See {@link LinkedBag} for the version from the
+ *  textbook that uses a non-static nested class.
+ *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
  *  take constant time. Iteration takes time proportional to the number of items.
  *  <p>
  *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
  */
 public class Bag<Item> implements Iterable<Item> {
-    private int N;         // number of elements in bag
-    private Node first;    // beginning of bag
+    private int N;               // number of elements in bag
+    private Node<Item> first;    // beginning of bag
 
     // helper linked list class
-    private class Node {
+    private class Node<Item> {
         private Item item;
-        private Node next;
+        private Node<Item> next;
     }
 
-   /**
-     * Create an empty stack.
+    /**
+     * Initializes an empty bag.
      */
     public Bag() {
         first = null;
         N = 0;
-        assert check();
     }
 
-   /**
-     * Is the BAG empty?
+    /**
+     * Is this bag empty?
+     * @return true if this bag is empty; false otherwise
      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the bag.
+    /**
+     * Returns the number of items in this bag.
+     * @return the number of items in this bag
      */
     public int size() {
         return N;
     }
 
-   /**
-     * Add the item to the bag.
+    /**
+     * Adds the item to this bag.
+     * @param item the item to add to this bag
      */
     public void add(Item item) {
-        Node oldfirst = first;
-        first = new Node();
+        Node<Item> oldfirst = first;
+        first = new Node<Item>();
         first.item = item;
         first.next = oldfirst;
         N++;
-        assert check();
     }
 
-    // check internal invariants
-    private boolean check() {
-        if (N == 0) {
-            if (first != null) return false;
-        }
-        else if (N == 1) {
-            if (first == null)      return false;
-            if (first.next != null) return false;
-        }
-        else {
-            if (first.next == null) return false;
-        }
 
-        // check internal consistency of instance variable N
-        int numberOfNodes = 0;
-        for (Node x = first; x != null; x = x.next) {
-            numberOfNodes++;
-        }
-        if (numberOfNodes != N) return false;
-
-        return true;
-    } 
-
-
-   /**
-     * Return an iterator that iterates over the items in the bag.
+    /**
+     * Returns an iterator that iterates over the items in the bag in arbitrary order.
+     * @return an iterator that iterates over the items in the bag in arbitrary order
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator();  
+        return new ListIterator<Item>(first);  
     }
 
     // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
 
         public boolean hasNext()  { return current != null;                     }
         public void remove()      { throw new UnsupportedOperationException();  }
@@ -131,8 +120,8 @@ public class Bag<Item> implements Iterable<Item> {
         }
     }
 
-   /**
-     * A test client.
+    /**
+     * Unit tests the <tt>Bag</tt> data type.
      */
     public static void main(String[] args) {
         Bag<String> bag = new Bag<String>();
--- stdlib/Queue.java
+++ stdlib/Queue.java
@@ -17,76 +17,86 @@ import java.util.NoSuchElementException;
  *  The <tt>Queue</tt> class represents a first-in-first-out (FIFO)
  *  queue of generic items.
  *  It supports the usual <em>enqueue</em> and <em>dequeue</em>
- *  operations, along with methods for peeking at the top item,
+ *  operations, along with methods for peeking at the first item,
  *  testing if the queue is empty, and iterating through
  *  the items in FIFO order.
  *  <p>
- *  All queue operations except iteration are constant time.
+ *  This implementation uses a singly-linked list with a static nested class for
+ *  linked-list nodes. See {@link LinkedQueue} for the version from the
+ *  textbook that uses a non-static nested class.
+ *  The <em>enqueue</em>, <em>dequeue</em>, <em>peek</em>, <em>size</em>, and <em>is-empty</em>
+ *  operations all take constant time in the worst case.
  *  <p>
  *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
  */
 public class Queue<Item> implements Iterable<Item> {
-    private int N;         // number of elements on queue
-    private Node first;    // beginning of queue
-    private Node last;     // end of queue
+    private int N;               // number of elements on queue
+    private Node<Item> first;    // beginning of queue
+    private Node<Item> last;     // end of queue
 
     // helper linked list class
-    private class Node {
+    private static class Node<Item> {
         private Item item;
-        private Node next;
+        private Node<Item> next;
     }
 
-   /**
-     * Create an empty queue.
+    /**
+     * Initializes an empty queue.
      */
     public Queue() {
         first = null;
         last  = null;
         N = 0;
-        assert check();
     }
 
-   /**
-     * Is the queue empty?
+    /**
+     * Is this queue empty?
+     * @return true if this queue is empty; false otherwise
      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the queue.
+    /**
+     * Returns the number of items in this queue.
+     * @return the number of items in this queue
      */
     public int size() {
         return N;     
     }
 
-   /**
-     * Return the item least recently added to the queue.
-     * @throws java.util.NoSuchElementException if queue is empty.
+    /**
+     * Returns the item least recently added to this queue.
+     * @return the item least recently added to this queue
+     * @throws java.util.NoSuchElementException if this queue is empty
      */
     public Item peek() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
         return first.item;
     }
 
-   /**
-     * Add the item to the queue.
+    /**
+     * Adds the item to this queue.
+     * @param item the item to add
      */
     public void enqueue(Item item) {
-        Node oldlast = last;
-        last = new Node();
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
         last.item = item;
         last.next = null;
         if (isEmpty()) first = last;
         else           oldlast.next = last;
         N++;
-        assert check();
     }
 
-   /**
-     * Remove and return the item on the queue least recently added.
-     * @throws java.util.NoSuchElementException if queue is empty.
+    /**
+     * Removes and returns the item on this queue that was least recently added.
+     * @return the item on this queue that was least recently added
+     * @throws java.util.NoSuchElementException if this queue is empty
      */
     public Item dequeue() {
         if (isEmpty()) throw new NoSuchElementException("Queue underflow");
@@ -94,12 +104,12 @@ public class Queue<Item> implements Iterable<Item> {
         first = first.next;
         N--;
         if (isEmpty()) last = null;   // to avoid loitering
-        assert check();
         return item;
     }
 
-   /**
-     * Return string representation.
+    /**
+     * Returns a string representation of this queue.
+     * @return the sequence of items in FIFO order, separated by spaces
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
@@ -108,51 +118,21 @@ public class Queue<Item> implements Iterable<Item> {
         return s.toString();
     } 
 
-    // check internal invariants
-    private boolean check() {
-        if (N == 0) {
-            if (first != null) return false;
-            if (last  != null) return false;
-        }
-        else if (N == 1) {
-            if (first == null || last == null) return false;
-            if (first != last)                 return false;
-            if (first.next != null)            return false;
-        }
-        else {
-            if (first == last)      return false;
-            if (first.next == null) return false;
-            if (last.next  != null) return false;
-
-            // check internal consistency of instance variable N
-            int numberOfNodes = 0;
-            for (Node x = first; x != null; x = x.next) {
-               numberOfNodes++;
-            }
-            if (numberOfNodes != N) return false;
-
-            // check internal consistency of instance variable last
-            Node lastNode = first;
-            while (lastNode.next != null) {
-               lastNode = lastNode.next;
-            }
-            if (last != lastNode) return false;
-        }
-
-        return true;
-    } 
- 
-
-   /**
-     * Return an iterator that iterates over the items on the queue in FIFO order.
+    /**
+     * Returns an iterator that iterates over the items in this queue in FIFO order.
+     * @return an iterator that iterates over the items in this queue in FIFO order
      */
     public Iterator<Item> iterator()  {
-        return new ListIterator();  
+        return new ListIterator<Item>(first);  
     }
 
     // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
 
         public boolean hasNext()  { return current != null;                     }
         public void remove()      { throw new UnsupportedOperationException();  }
@@ -166,8 +146,8 @@ public class Queue<Item> implements Iterable<Item> {
     }
 
 
-   /**
-     * A test client.
+    /**
+     * Unit tests the <tt>Queue</tt> data type.
      */
     public static void main(String[] args) {
         Queue<String> q = new Queue<String>();
