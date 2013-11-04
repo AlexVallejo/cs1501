2c62d32a2f3973dda1871ed7a36d3e232163755e
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-03 23:01:59 -0500

    Add filename constructor to network

--- assignment-4/src/Network.java
+++ assignment-4/src/Network.java
@@ -6,9 +6,15 @@
  * Peoplesoft: 357-8411
  */
 
-public class Network extends EdgeWeightedGraph{
+public class Network extends EdgeWeightedGraph {
 
-
+  /**
+   * Construct a new network from the given filename
+   * @param filename the name of the input file
+   */
+  public Network(String filename){
+    super(filename);
+  }
 
   public void up(int vertex1, int vertex2, int latency){
     Edge e = new Edge(vertex1,vertex1,latency);
