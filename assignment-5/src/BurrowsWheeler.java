460a2f54c577824b20ad536765fbdb0b1a5d0c1c
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-14 01:15:18 -0500

    Add skeleton files for assignment-5

---
+++ assignment-5/src/BurrowsWheeler.java
@@ -0,0 +1,24 @@
+/**
+ * Created By: Alex Vallejo
+ * Date: 11/14/13
+ * Project: assignment-5
+ * Email: amv49@pitt.edu
+ * Peoplesoft: 357-8411
+ */
+public class BurrowsWheeler {
+  // apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
+  public static void encode(){
+
+  }
+
+  // apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
+  public static void decode(){
+
+  }
+
+  // if args[0] is '-', apply Burrows-Wheeler encoding   - 5 points
+  // if args[0] is '+', apply Burrows-Wheeler decoding   - 5 points
+  public static void main(String[] args){
+
+  }
+}
---
+++ assignment-5/src/CircularSuffixArray.java
@@ -0,0 +1,24 @@
+/**
+ * Created By: Alex Vallejo
+ * Date: 11/14/13
+ * Project: assignment-5
+ * Email: amv49@pitt.edu
+ * Peoplesoft: 357-8411
+ */
+public class CircularSuffixArray {
+
+  // circular suffix array of s   - 10 points
+  public CircularSuffixArray(String s){
+
+  }
+
+  // length of s
+  public int length(){
+    return 0;
+  }
+
+  // returns index of ith sorted suffix - 10 points
+  public int index(int i){
+    return i;
+  }
+}
