04aed5c71a8dc6fac300bb406454cb16fc928d38
Alex Vallejo <vallejo.alex@gmail.com>
2013-11-29 20:18:07 -0500

    Add documentation and refactor CSA for readability. Add extra test file, amendments.txt. Compression rate for amendments appears to be ~40%

--- assignment-5/CircularSuffixArray.java
+++ assignment-5/CircularSuffixArray.java
@@ -19,7 +19,10 @@ public class CircularSuffixArray {
 
   private Suffix[] suffixes;
 
-  // circular suffix array of s   - 10 points
+  /**
+   * The constructor instantiates a new suffix for each character in standard
+   * input and sorts the suffixes.
+   */
   public CircularSuffixArray(String s){
 
     int length = s.length();
@@ -29,24 +32,76 @@ public class CircularSuffixArray {
       suffixes[i] = new Suffix(s,i);
 
     Arrays.sort(suffixes);
-
   }
 
+  /**
+   * Returns the number of suffixes in the CSA. Equal to the number of
+   * characters in the original text.
+   *
+   * @return number if suffixes in the CSA
+   */
+  public int length(){
+    return suffixes.length;
+  }
+
+  /**
+   * Returns the index of the ith sorted suffix
+   *
+   * @param i ith sorted suffix to find
+   * @return index of the ith sorted suffix
+   */
+  public int index(int i){
+    return suffixes[i].length();
+  }
+
+  /**
+   * Inner class that holds the data for each suffix. This class is efficient
+   * in the way that it deals with the suffixes because it does not actually
+   * generate the suffix using substring. It simply stores the begining index
+   * of the suffix it is representing and returns the begining character
+   * only when asked.
+   *
+   * The begining character is also the character at the index in the orignal 
+   * string
+   */
   private static class Suffix implements Comparable<Suffix> {
     private final String text;
     private final int index;
 
+    /**
+     * Store the original string and the index into the string representing the
+     * first character in the suffix
+     */
     private Suffix(String text, int index) {
       this.text  = text;
       this.index = index;
     }
+
+    /**
+     * Returns the length of the suffix
+     *
+     * @return length of this suffix
+     */
     private int length() {
       return text.length() - index;
     }
+
+    /**
+     * Find the character at the ith position in the suffix
+     *
+     * @return ith char of the suffix
+     */
     private char charAt(int i) {
       return text.charAt(index + i);
     }
 
+    /**
+     * Implement the comparable interface
+     *
+     * @return -1 if the calling suffix appears lexographically before the
+     *          argument suffix. 1 if the calling suffix appears lexographically
+     *          after the argument suffix. 0 if the two suffixes are equal.
+     */
     public int compareTo(Suffix that) {
 
       if (this == that) return 0;  // optimization
@@ -64,18 +119,11 @@ public class CircularSuffixArray {
       return this.length() - that.length();
     }
 
+    /**
+     * object toString override
+     */
     public String toString() {
       return text.substring(index);
-    }
-  }
-
-  // length of s
-  public int length(){
-    return suffixes.length;
-  }
-
-  // returns index of ith sorted suffix - 10 points
-  public int index(int i){
-    return suffixes[i].length();
-  }
-}
+    }// end Suffix.toString
+  }//end inner class Suffix
+}//end class CSA
